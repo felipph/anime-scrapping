38311eac5327f23f2ddcd48865139a0f30c3a289
Felipph Calado <luizfelipph@gmail.com>
2022-09-18 13:25:22 -0300

    Sem paralelismo

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,6 @@ import * as AWS from 'aws-sdk';
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 exports.lambdaHandler = async function (payload: LambdaPayload, context: any) {
-  const concurrentItens = 5;
-
   const pagesToFetch = Number(
     payload.maxPages !== undefined ? payload.maxPages : process.env['MAX_PAGES']
   );
@@ -14,52 +12,32 @@ exports.lambdaHandler = async function (payload: LambdaPayload, context: any) {
 
   const service = new AnimesGrastisBRService();
 
-  const loops = Math.round(pagesToFetch / concurrentItens) + 1;
-  const allResult = [];
-  let totalPagesFetched = 1;
-  for (let j = 0; j < loops; j++) {
-    const pages = [];
-    for (let i = 1; i <= 5; i++) {
-      if (totalPagesFetched > pagesToFetch) {
-        break;
-      }
-      pages.push(service.getPage(String(i)));
-      totalPagesFetched++;
-    }
-    if (pages.length > 0) {
-      await Promise.all(pages).then(async results => {
-        for (const page of results) {
-          allResult.push(...page);
-          const putReqs = page.map((item: any) => ({
-            PutRequest: {
-              Item: item,
-            },
-          }));
-          const req = {
-            RequestItems: {
-              AnimeList: putReqs,
-            },
-          };
-          console.log('RequestItems');
-          console.log(req);
-          console.log(req.RequestItems.AnimeList.length);
-          await dynamoDB.batchWrite(req, (err, data) => {
-            if (err) {
-              console.log(err);
-              throw err;
-            } else {
-              console.log('Página OK');
-              console.log(data);
-            }
-          });
-        }
-      });
-    }
+  const pages = [];
+  for (let i = 1; i <= pagesToFetch; i++) {
+    pages.push(this.getPage(String(i)));
   }
+  const allResult = [];
+  await Promise.all(pages).then(async results => {
+    for (const page of results) {
+      allResult.push(...page);
+      const putReqs = page.map((item: any) => ({
+        PutRequest: {
+          Item: item,
+        },
+      }));
+      const req = {
+        RequestItems: {
+          AnimeList: putReqs,
+        },
+      };
+      console.log('RequestItems');
+      console.log(req);
+      await dynamoDB.batchWrite(req).promise();
+    }
+  });
 
   return {
     size: allResult.length,
     params: payload,
-    totalPagesFetched: totalPagesFetched,
   };
 };
